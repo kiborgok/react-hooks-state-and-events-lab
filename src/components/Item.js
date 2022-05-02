ff72b8423ba3829e88b575dca9b83bc8fef7b8b0
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-02 17:13:33 +0300

    add useState hook

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        className={inCart ? "remove" : "add"}
+        onClick={() => setInCart((inCart) => !inCart)}
+      >
+        {inCart ? "Remove from " : "Add to "}Cart
+      </button>
     </li>
-  );
+  );  
 }
 
 export default Item;
