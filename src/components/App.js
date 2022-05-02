a1b895d1a43ecf62f0fc4f712cd5e3811f89e349
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-02 17:14:22 +0300

    add useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import items from "../data/items";
 
 function App() {
-
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [appClass, setAppClass] = useState(false);
 
   return (
-    <div className={appClass}>
+    <div className={appClass ? "App dark" : "App light"}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={() => setAppClass(appClass => !appClass)}>{appClass ? "Light Mode" : "Dark Mode"}</button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={items} />
     </div>
   );
 }
