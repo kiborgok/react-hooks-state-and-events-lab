8ec7ef088dc29bf1c3449d78f4a0647c7699d757
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-02 17:13:50 +0300

    add useState hook

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterValue, setFilterValue] = useState("All");
+  function handleChange(category) {
+    setFilterValue(category);
+  }
+  const data = items.filter((item) => {
+    return filterValue === "All" ? item : item.category === filterValue;
+  });
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e) => handleChange(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +20,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {data.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
